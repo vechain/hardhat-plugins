5a23565f3e2531ced8fa92ae03aef66558c53f1b
Darren Kelly <darren.kelly@vechain.org>
2023-10-19 17:53:25 +0100

    refactor(clause-builder): accepting function data in the clause builder

--- packages/vechain/test/clausesBuilder.test.ts
+++ packages/vechain/test/clausesBuilder.test.ts
@@ -1,10 +1,18 @@
-import { HttpNetworkConfig } from "hardhat/types";
-import { createProvider } from "../src/helpers/createProvider";
-import { createWallet } from "../src/helpers/createWallet";
-import { ClausesBuilder } from "../src/clausesBuilder";
-import { BaseContract } from "ethers";
-import { Provider } from "@vechain/web3-providers-connex";
+import {HttpNetworkConfig} from "hardhat/types";
+import {createProvider} from "../src/helpers/createProvider";
+import {createWallet} from "../src/helpers/createWallet";
+import {ClausesBuilder} from "../src/clausesBuilder";
+import {BaseContract} from "ethers";
+import {Provider} from "@vechain/web3-providers-connex";
 
+const baseContract = {
+  interface: {
+    encodeFunctionData: jest.fn().mockImplementation(() => {
+      return 'fakeFunctionData';
+    })
+  },
+  getAddress: jest.fn().mockReturnValue("0x0000000000000000000000000000456E65726779")
+} as unknown as BaseContract;
 
 describe('builder tests', () => {
 
@@ -29,46 +37,31 @@ describe('builder tests', () => {
 
   it('should add clause', async () => {
 
-    const baseContract = jest.fn().mockImplementation(() => {
-      return null;
-    }) as unknown as BaseContract;
-
     var wallet = createWallet(config);
     wallet.import("0x99f0500549792796c14fed62011a51081dc5b5e68fe8bd8a13b86be829c4fd36");
 
     var provider = await createProvider(config, wallet);
 
     const clauseBuilder = new ClausesBuilder(baseContract, provider);
-    const builderWithClause = clauseBuilder.withClause({
-      args: [1],
-      abi: 'fakeAbi',
-      method: 'fakeMethod'
-    })
-    expect(builderWithClause['clauses']).toEqual([{ abi: 'fakeAbi', args: [1], method: 'fakeMethod' }]);
+    const builderWithClause = clauseBuilder.withClause(
+      baseContract.interface.encodeFunctionData("mint", ["0xf077b491b355E64048cE21E3A6Fc4751eEeA77fa"])
+    )
+    expect(builderWithClause['functionCalls']).toEqual(["fakeFunctionData"]);
   });
 
   it('should add two clauses', async () => {
-
-    const baseContract = jest.fn().mockImplementation(() => {
-      return null;
-    }) as unknown as BaseContract;
 
     var wallet = createWallet(config);
     var provider = await createProvider(config, wallet);
 
     const clauseBuilder = new ClausesBuilder(baseContract, provider);
-    const builderWithClause = clauseBuilder.withClause({
-      args: [1],
-      abi: 'fakeAbi',
-      method: 'fakeMethod'
-    }).withClause({
-      args: [2],
-      abi: 'fakeAbi2',
-      method: 'fakeMethod2'
-    })
-    expect(builderWithClause['clauses']).toEqual([
-      { abi: 'fakeAbi', args: [1], method: 'fakeMethod' },
-      { abi: 'fakeAbi2', args: [2], method: 'fakeMethod2' }
+    const builderWithClause = clauseBuilder
+      .withClause(baseContract.interface.encodeFunctionData("mint", ["0xf077b491b355E64048cE21E3A6Fc4751eEeA77fa"]))
+      .withClause(baseContract.interface.encodeFunctionData("mint", ["0xf077b491b355E64048cE21E3A6Fc4751eEeA77fa"]))
+
+    expect(builderWithClause['functionCalls']).toEqual([
+      "fakeFunctionData",
+      "fakeFunctionData"
     ]);
   });
 
@@ -108,17 +101,13 @@ describe('builder tests', () => {
       }
     } as unknown as Provider;
 
-    const baseContract = {
-      getAddress: jest.fn().mockReturnValue("0x0000000000000000000000000000456E65726779")
-    } as unknown as BaseContract;
-
     const clauseBuilder = new ClausesBuilder(baseContract, connexProviderWrapper);
 
-    const tx = await clauseBuilder.withClause({
-      args: [1],
-      abi: JSON.stringify([{ type: 'function', name: 'fakeMethod'}] ),
-      method: 'fakeMethod'
-    }).send()
+    const tx = await clauseBuilder
+      .withClause(
+        baseContract.interface.encodeFunctionData("mint", ["0xf077b491b355E64048cE21E3A6Fc4751eEeA77fa"])
+      )
+      .send()
     expect(tx).toEqual(expectedTxResponse);
   });
 
@@ -133,17 +122,13 @@ describe('builder tests', () => {
       jest.spyOn(resolved, 'request').mockResolvedValue(dummyResponse);
     });
 
-    const baseContract = {
-      getAddress: jest.fn().mockReturnValue("0x0000000000000000000000000000456E65726779")
-    } as unknown as BaseContract;
-
     const clauseBuilder = new ClausesBuilder(baseContract, invalidConnexProviderWrapper);
     try {
-      await clauseBuilder.withClause({
-        args: [1],
-        abi: JSON.stringify([{ type: 'function', name: 'fakeMethod'}] ),
-        method: 'fakeMethod'
-      }).send()
+      await clauseBuilder
+        .withClause(
+          baseContract.interface.encodeFunctionData("mint", ["0xf077b491b355E64048cE21E3A6Fc4751eEeA77fa"])
+        )
+        .send()
     } catch (e) {
       const error = e as Error;
       expect(error.message).toEqual('vechain hardhat plugin requires vechain provider for clauses operation')
