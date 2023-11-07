33e3483a325ed0fabfe270cf12f5ed128bc9fbfb
Andreas Papageorgiou <andreas@btblock.io>
2023-11-07 10:30:37 +0200

    overwrite provider.getNetwork

--- packages/ethers/src/index.ts
+++ packages/ethers/src/index.ts
@@ -16,11 +16,8 @@ import {
     deployContract
 } from "@nomicfoundation/hardhat-ethers/internal/helpers";
 import { HardhatEthersProvider as HardhatEthersProviderT } from "@nomicfoundation/hardhat-ethers/internal/hardhat-ethers-provider";
-import { modifyFactory, modifyProvider } from "@vechain/web3-providers-connex/dist/ethers";
+import { modifyFactory } from "@vechain/web3-providers-connex/dist/ethers";
 
-import * as thor from '@vechain/web3-providers-connex'
-import { Framework } from '@vechain/connex-framework'
-import { Driver, SimpleNet, SimpleWallet } from '@vechain/connex-driver'
 extendEnvironment(hre => {
     if (hre.vechain === undefined) {
         if (hre.network.name.includes("vechain")) {
@@ -36,10 +33,42 @@ extendEnvironment(hre => {
             HardhatEthersProvider: typeof HardhatEthersProviderT;
         }
 
-        const provider = new HardhatEthersProvider(
+        var provider = new HardhatEthersProvider(
             hre.vechain!,
             hre.network.name
         )
+
+        provider.getNetwork = async function() {
+            const chainId = await this.send("eth_chainId", []);
+            const hexChainId = chainId.toString(16);
+            return new ethers.Network(hre.network.name, "0x" + hexChainId);
+        };
+
+        provider.getSigner = async (address?: string | number | undefined) => {
+            if (address === null || address === undefined) {
+                address = 0;
+            }
+            const accountsPromise = provider.send("eth_accounts", []);
+
+            // Account index
+            if (typeof address === "number") {
+                const accounts = await accountsPromise;
+                if (address >= accounts.length) {
+                    throw new Error(`Address index out of bounds: ${address}`);
+                }
+                
+                let defaultSigner = await getSigner(hre, accounts[address]);
+
+                defaultSigner.signTransaction = async (transaction: any) => {
+                    return hre.vechain!.sign(transaction);
+                }
+                return defaultSigner;
+            }
+            else {
+                // If the address is a string, you might want to handle it differently or throw another error.
+                throw new Error(`Unsupported address type: ${address}`);
+            }
+        }
 
         return {
             ...ethers,
